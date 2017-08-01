73db6033d19b6d672ecacaa408a7179f0bd0aa86
Sam <swkane@indiana.edu>
2017-08-01 16:16:13 -0400

    Page build complete. Some styling to be completed

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 console.log(customers);
-console.log(customers.results[0].email);
-let container = document.querySelector('.container');
-// let image = document.createElement('img');
-// image.setAttribute('src', customers.results[0].picture.large);
-// container.appendChild(image);
-let profile = document.createElement('div');
-profile.setAttribute('class', 'profile');
-// shorthand variable
-const results = customers.results[0];
-profile.innerHTML = `
-  <img src="${results.picture.large}" alt="profile">
-  <p class="name">${results.name.first} ${results.name.last}</p>
-  <p class="email">${results.email}</p>
-  <p class="address">${results.location.street}</p>
-  <p class="address">${results.location.city}, ${results.location.state} ${results.location.postcode}</p>
-  <p class="number">${results.phone}</p>
+let main = document.querySelector('.main');
 
+for (let i = 0; i < customers.results.length; i++) {
+  let profile = document.createElement('div');
+  profile.setAttribute('class', 'profile');
+  // shorthand variable
+  const results = customers.results[i];
+  profile.innerHTML = `
+    <img src="${results.picture.large}" alt="profile">
+    <div class="info">
+      <p class="name">${results.name.first} ${results.name.last}</p>
+      <p class="email">${results.email}</p>
+      <p class="address">${results.location.street}</p>
+      <p class="address">${results.location.city}, ${results.location.state} ${results.location.postcode}</p>
+      <p class="address">${results.phone}</p>
+      <p class="id">${results.id.value}</p>
+    </div>
+  `;
 
-
-
-`;
-
-container.appendChild(profile);
+  main.appendChild(profile);
+}
